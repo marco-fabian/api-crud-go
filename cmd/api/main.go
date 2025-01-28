5280b8eafc7809ff847198404c007bd637b852a0
Marco <marcofabianufmg@hotmail.com>
2025-01-28 10:42:46 -0300

    [feat] created repository structure

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/marco-fabian/api-crud-go/internal/database"
+	"github.com/marco-fabian/api-crud-go/internal/post"
 )
 
 func main() {
+	// Estabelecendo uma conexão com o banco de dados PostgreSQL
 	connectionString := "postgresql://posts:p0stgr3s@db:5432/posts"
-	_, err := database.NewConnection(connectionString)
+	conn, err := database.NewConnection(connectionString)
 	if err != nil {
 		panic(err)
 	}
 
+	defer conn.Close()
+
+	repo := post.Repository{
+		Conn: conn,
+	}
+	repo = repo
+
+	// Definindo rota para o endpoint via GET
 	g := gin.Default()
 	g.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
---
+++ internal/post.go
@@ -0,0 +1,11 @@
+package internal
+
+import (
+	"time"
+)
+
+type Post struct {
+	Username  string    `json:"username"`
+	Body      string    `json:"body"`
+	CreatedAt time.Time `json:"created_at"`
+}
